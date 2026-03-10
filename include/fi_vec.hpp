b24ee35962f425379fdbfc0bc4f2728aa555533e
RAGREON <ragreon809@gmail.com>
2026-03-10 09:51:00 +0545

    refactor: change data name and access modifier order

--- include/fi_vec.hpp
+++ include/fi_vec.hpp
@@ -9,97 +9,103 @@
 namespace fi {
   template <class T, class Allocator = std::allocator<T>>
   class vec {
-  public:
-    vec() : count(0), containerCapacity(0), container(nullptr) {}
+  private:
+    T* data_;
+    size_t size_;
+    size_t capacity_;
+    Allocator alloc_;
 
-    vec(size_t n) : count(0), containerCapacity(n) {
-      container = static_cast<T*>(::operator new(n * sizeof(T)));
+  public:
+    vec() : size_(0), capacity_(0), data_(nullptr) {}
+
+    vec(size_t n) : size_(0), capacity_(n) {
+      data_ = alloc_.allocate(n);
     }
 
-    vec(size_t n, T value) : count(n) {
-      containerCapacity = static_cast<int>(n * 1.5);
-      container = static_cast<T*>(::operator new(containerCapacity * sizeof(T)));
+    vec(size_t n, T value) : size_(n) {
+      capacity_ = static_cast<int>(n * 1.5);
+      data_ = static_cast<T*>(::operator new(capacity_ * sizeof(T)));
 
       for (size_t i = 0; i < n; i++) 
-        ::new (container + i) T(value);
+        ::new (data_ + i) T(value);
 
-      count = n;
+      size_ = n;
     }
 
     vec(const std::initializer_list<T>& elements) {
-      count = elements.size();
-      containerCapacity = (containerCapacity == 0) ? 1 : static_cast<size_t>(count * 1.5);
-      container = static_cast<T*>(::operator new(containerCapacity * sizeof(T)));
+      size_ = elements.size();
+      capacity_ = (capacity_ == 0) ? 1 : static_cast<size_t>(size_ * 1.5);
+      data_ = static_cast<T*>(::operator new(capacity_ * sizeof(T)));
 
       const T* ptr = elements.begin();
 
       for (size_t i = 0; i < elements.size(); i++)
-        ::new (container + i) T(ptr[i]);
+        ::new (data_ + i) T(ptr[i]);
     }
 
-    vec(const vec& other) : count(other.count), containerCapacity(other.containerCapacity) {
-      container = static_cast<T*>(::operator new(containerCapacity * sizeof(T)));
-      for (size_t i = 0; i < count; i++)
-        ::new (container + i) T(other.container[i]);
+    vec(const vec& other) : size_(other.size_), capacity_(other.capacity_) {
+      data_ = static_cast<T*>(::operator new(capacity_ * sizeof(T)));
+      for (size_t i = 0; i < size_; i++)
+        ::new (data_ + i) T(other.data_[i]);
     }
 
     void push_back(T element) {
-      if ((count + 1) >= containerCapacity) {
-        containerCapacity = static_cast<size_t>(containerCapacity * 1.5);
+      if ((size_ + 1) >= capacity_) {
+        capacity_ = static_cast<size_t>(capacity_ * 1.5);
 
-        T *newContainer = static_cast<T*>(::operator new(containerCapacity * sizeof(T)));
+        T *newdata_ = static_cast<T*>(::operator new(capacity_ * sizeof(T)));
 
-        for (size_t i = 0; i < count; i++) {
-          ::new (newContainer + i) T(std::move(container[i]));
+        for (size_t i = 0; i < size_; i++) {
+          ::new (newdata_ + i) T(std::move(data_[i]));
         }
 
-        for (size_t i = 0; i < count; i++) {
-          container[i].~T();
+        for (size_t i = 0; i < size_; i++) {
+          data_[i].~T();
         }
-        ::operator delete(container);
+        ::operator delete(data_);
 
-        container = newContainer;
+        data_ = newdata_;
       }
 
-      ::new (container + count++) T(std::move(element));
+      ::new (data_ + size_++) T(std::move(element));
     }
 
     T& at(const size_t index) {
-      if (index >= count) {
+      if (index >= size_) {
         std::cerr << "invalid index" << std::endl;
         exit(1);
       }
       
-      return container[index];
+      return data_[index];
     }
 
     T& at(const size_t index) const {
-      if (index >= count) {
+      if (index >= size_) {
         std::cerr << "invalid index" << std::endl;
         exit(1);
       }
 
-      return container[index];
+      return data_[index];
     }
   
     size_t size() const {
-      return count;
+      return size_;
     }
 
     size_t capacity() const {
-      return containerCapacity;
+      return capacity_;
     }
 
 #ifdef LIB_TEST
-    T* getContainer() {
-      return this->container;
+    T* getData() {
+      return this->data_;
     }
 #endif
 
     vec& operator=(vec other) {
-      std::swap(count, other.count);
-      std::swap(containerCapacity, other.containerCapacity);
-      std::swap(container, other.container);
+      std::swap(size_, other.size_);
+      std::swap(capacity_, other.capacity_);
+      std::swap(data_, other.data_);
       return *this; 
     }
 
@@ -112,16 +118,11 @@ namespace fi {
     }
 
   ~vec() {
-    for (size_t i = 0; i < count; i++) {
-      container[i].~T();
+    for (size_t i = 0; i < size_; i++) {
+      data_[i].~T();
     }
-    ::operator delete(container);
+    ::operator delete(data_);
   }
 
-  private:
-    T *container;
-    
-    size_t count;
-    size_t containerCapacity;
   };
 }
